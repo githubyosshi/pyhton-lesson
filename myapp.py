9107427d7bdf82127fb27aec4c606d313a0368df
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-25 15:20:48 +0900

    variable commit

--- myapp.py
+++ myapp.py
@@ -5,4 +5,13 @@ Comment
 Comment
 """
 # Comment
-print("Hell World")
+# print("Hell World")
+
+# 変数
+msg = "Hell World"
+print(msg)
+msg = "Hell Again"
+print(msg)
+
+# 定数
+ADMIN_EMAIL = "taguchi@gmail.com"
