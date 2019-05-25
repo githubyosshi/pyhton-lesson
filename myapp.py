c63c03ee1b8160e96c9c3d69625f9bc0f270725b
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-25 16:01:02 +0900

    Handle various data commit

--- myapp.py
+++ myapp.py
@@ -15,3 +15,20 @@ print(msg)
 
 # 定数
 ADMIN_EMAIL = "taguchi@gmail.com"
+
+# 文字列
+s = "he\nllo wor\tld"
+html = """<html>
+<body></body>
+</html>"""
+print(s)
+print(html)
+
+# 整数
+i = 10
+
+# 浮動小数点
+f = 23.4
+
+# 論理値
+flag = True # false
