c08a0aab285b34e98f9da3aedbe1abe05d972231
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-05-26 12:27:49 +0900

    Embedding String Values commit

--- myapp.py
+++ myapp.py
@@ -54,3 +54,14 @@ print(not True) # False
 print("hello " + "word")
 print("hello " * 3)
 
+# 文字列に値を埋め込み %s:文字列 %f:浮動小数点 %d整数　その後に%を記述し埋め込みたい値を記述
+name = "taguchi"
+score = 52.8
+
+# print("name: %s, sore: %f" % (name, score))
+# -左揃え10巾、
+# print("name: %-10s, sore: %10.2f" % (name, score))
+# 0番目、1番目の要素
+# print("name: {0}, score: {1}".format(name, score))
+# >左揃え桁数、<右揃え桁数
+print("name: {0:>10s}, score: {1:<10.2f}".format(name, score))
